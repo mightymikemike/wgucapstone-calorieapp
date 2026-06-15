dd21e9a61ee162dd57ec930409fe0a50376e8c50
mightymikemike <michaeljohnson2e@gmail.com>
2026-06-15 17:02:02 -0400

    created Main.java and pom.xml (may need to update)

---
+++ src/main/Main.java
@@ -0,0 +1,40 @@
+package main;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+//import javafx.scene.chart.LineChart;
+
+public class Main extends Application {
+
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Calorie App");
+        button = new Button("Click Me");
+        //button.setText("Button Text")
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+
+    }
+
+}
+
+// https://www.youtube.com/watch?v=FLkOX4Eez6o
